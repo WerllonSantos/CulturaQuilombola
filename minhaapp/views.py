ad6ed0466f71a1c83e596d380c5c6c00a19f973d
WerllonSantos <paulowerllon@hotmail.com>
2024-02-19 12:12:30 -0300

    nova atualização

---
+++ minhaapp/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def pagina (request):
+    return render(request, 'pagina.html')
+def sobre (request):
+    return render(request, 'sobre.html')
+
+def equipe (request):
+    return render(request, 'equipe.html')
+
+def atividades (request):
+    return render(request, 'atividades.html')
+
+
+
+def contato (request):
+    return render(request, 'contato.html')
