4249cb0daf023179d5f58bc019d18cb2eba13808
WerllonSantos <paulowerllon@hotmail.com>
2024-03-22 23:27:46 -0300

    pagina, doar, eventos, noticias e outros

--- minhaapp/urls.py
+++ minhaapp/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
-from .views import pagina, sobre,equipe, atividades,voluntario, contato
+from .views import pagina, sobre,equipe, noticias, eventos, voluntario,doar, contato
 
 urlpatterns = [
     path('', pagina, name="pagina"),
-    path('sobre/',sobre, name="sobre"),
+    path('sobre/', sobre, name="sobre"),
     path('equipe/', equipe, name="equipe"),
-    path('atividades/', atividades, name="atividades"),
+    path('noticias/', noticias, name="noticias"),
+    path('eventos/', eventos, name="eventos"),
     path('voluntario/', voluntario, name="voluntario"),
+    path('doar/', doar, name="doar"),
     path('contato/', contato, name="contato"),
 
 ]
