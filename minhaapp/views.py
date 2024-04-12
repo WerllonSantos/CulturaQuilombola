a6e51bd2e5190e7894da704277cc7f3482731434
WerllonSantos <paulowerllon@hotmail.com>
2024-04-12 00:16:44 -0300

    reoganizado

--- minhaapp/views.py
+++ minhaapp/views.py
@@ -10,10 +10,17 @@ def sobre (request):
 def equipe (request):
     return render(request, 'equipe.html')
 
-def atividades (request):
-    return render(request, 'atividades.html')
+def noticias (request):
+    return render(request, 'noticias.html')
 
+def eventos (request):
+    return render(request, 'eventos.html')
 
+def voluntario (request):
+    return render(request, 'voluntario.html')
+
+def doar (request):
+    return render(request, 'doar.html')
 
 def contato (request):
     return render(request, 'contato.html')
